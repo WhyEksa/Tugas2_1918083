75b347d363656afcbf3d6b79531df61c5cf9f8bb
WhyEksa <wahyu.eka.saputra186@gmail.com>
2022-10-17 07:55:34 +0700

    Fragment

---
+++ app/src/main/java/com/wes/pertemuan2_tugas_fragment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wes.pertemuan2_tugas_fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
